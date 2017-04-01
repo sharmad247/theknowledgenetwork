b503461adb7c951057fd1211b30590b9edbf57dc
Douglas Vaz <douglas1992@gmail.com>
2017-04-01 12:36:29 +0100

    Prepare to use Pubmed as a module

--- pubmed.js
+++ pubmed.js
@@ -1,10 +1,10 @@
 var request = require('request');
 var xml2js = require('xml2js');
 
+const pubmedBase = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils'
 
-function search(term, done)
-{
-    request('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='+term+'&reldate=60&datetype=edat&retmax=100&usehistory=y&retmode=JSON', function (error, response, body) {
+function search(term, done) {
+    request(pubmedBase + '/esearch.fcgi?db=pubmed&term='+term+'&reldate=60&datetype=edat&retmax=100&usehistory=y&retmode=JSON', function (error, response, body) {
         var result = JSON.parse(body);
         done(result.esearchresult.idlist);
     });
@@ -14,15 +14,17 @@ function fetchAbstracts(ids, done) {
     var idString = ids.join(',');
     var parser = new xml2js.Parser();
 
-    request('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id='+ idString + '&retmode=xml&rettype=abstract', function(error, reponse, body){
+    request(pubmedBase + '/efetch.fcgi?db=pubmed&id='+ idString + '&retmode=xml&rettype=abstract', function(error, reponse, body){
         var result = parser.parseString(body, function(err, result){
             done(result);
         })
     })
 }
 
-search('cancer', function(ids){
+search('cancer', function(ids) {
     fetchAbstracts(ids, function(abs){
         console.log(abs.PubmedArticleSet.PubmedArticle[0]); 
     });
 });
+
+module.exports = search
