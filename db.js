f649e02411eb2ca43f729febc5ed0cdea727c825
Douglas Vaz <douglas1992@gmail.com>
2017-04-02 04:21:51 +0100

    Fix package dependencies; handle db connection fail

--- db.js
+++ db.js
@@ -6,31 +6,31 @@ var connection = mysql.createConnection({
     password: 'admin',
     database: 'icmr'
 });
-connection.connect();
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
 function validate(username, password, done) {
-    connection.query("SELECT * FROM `user` WHERE `email` = '" + username + "'",function(err,rows){
+  connection.query("SELECT * FROM `user` WHERE `email` = '" + username + "'", function(err,rows) {
+    if (err) {
+    console.error(err);
+    return done(false);
+   }
+    if (!rows.length) {
+    console.log('loginMessage', 'No user found.');
+    return done(false);
+   }
 
-
-       if (err)
-       {
-            console.error(err);
-           return done(false);
-       }
-        if (!rows.length) {
-            console.log('loginMessage', 'No user found.'); // req.flash is the way to set flashdata using connect-flash
-            return done(false); // req.flash is the way to set flashdata using connect-flash
-       }
-
-        console.log(rows[0].password, password);
-
-       // if the user is found but the password is wrong
-       if ( rows[0].password == password) {
-        return done(true); // create the loginMessage and save it to session as flashdata
-        }
-        console.log('loginMessage', 'Oops! Wrong password.'); // create the loginMessage and save it to session as flashdata
-       return done(false);
-   });
+   if ( rows[0].password == password) 
+      done(true);
+    else
+      done(false);
+  });
 }
 
 exports.validate = validate
